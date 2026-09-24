d913617b64bbf2de35fc65645684caa5cce0cdbf
JinalChoudhari <142147469+JinalChoudhari@users.noreply.github.com>
2026-09-24 15:19:34 +0530

    Another Java basic program

--- src/basics/Calculator.java
+++ src/basics/Calculator.java
@@ -1,4 +1,35 @@
 package basics;
-
+import java.util.Scanner;
 public class Calculator {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double a = sc.nextDouble();
+        System.out.println("Enter second number: ");
+        double b = sc.nextDouble();
+        System.out.println("Enter the operator: ");
+        char op = sc.next().charAt(0);
+
+        switch(op){
+            case '+':
+            System.out.println("Result: " + (a + b));
+            break;
+            case '-':
+                System.out.println("Result: " + (a - b));
+                break;
+            case '*':
+                System.out.println("Result: " + (a * b));
+                break;
+            case '/':
+                if (b != 0)
+                    System.out.println("Result: " + (a / b));
+                else
+                    System.out.println("Cannot divide by zero.");
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+sc.close();
+    }
+
 }
---
+++ src/basics/GradeCalculator.java
@@ -0,0 +1,31 @@
+package basics;
+import java.util.Scanner;
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your marks:");
+        double marks = sc.nextDouble();
+        if (marks >= 90) {
+System.out.println("Grade A+");
+        }
+        else if(marks >=80){
+System.out.println("Grade A");
+        }
+        else if(marks >=70){
+            System.out.println("Grade B");
+        }
+        else if(marks >=60){
+            System.out.println("Grade C");
+        }
+        else if(marks >=50){
+            System.out.println("Grade D");
+        }
+        else if(marks >=40){
+            System.out.println("Grade E ");
+        }
+        else{
+            System.out.println("Grade F");
+        }
+        sc.close();
+    }
+}
